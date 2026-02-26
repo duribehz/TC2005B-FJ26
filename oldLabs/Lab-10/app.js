ed297458293687d4dc85316e6f75db18ae4593dc
Darío A. <dario.uribe.bss@gmail.com>
2026-02-25 23:29:18 -0600

    main route and serve index

--- oldLabs/Lab-10/app.js
+++ oldLabs/Lab-10/app.js
@@ -1,12 +1,40 @@
 const http = require('http');
 
+const html_header = `
+    <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Playlist</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+            </head>
+            <body>
+                <section class="section">
+                <div class="container">
+                <h1 class="title">
+                    <a href="/">Playlist</a>
+                </h1>
+                <p class="subtitle">Agrega tus canciones favoritas</p>
+`;
+
+const html_footer = `
+    </div>
+    </section>
+    </body>
+    </html>
+`;
+
+
+
 const server = http.createServer((request, response) => {
-    if (request.url === '/') {
+    if (request.url === '/' && request.method === "GET") {
         let html_index = `
-            <h1>Hello World!!</h1>
+            <a class="button is-link mb-4" href="/new">Agregar canción</a>
+            <div class="columns is-multiline">
         `;
         response.setHeader('Content-Type','text/html');
-        response.write(html_index);
+        response.write(html_header + html_index + html_footer);
         response.end();
     }
     else{
@@ -16,4 +44,6 @@ const server = http.createServer((request, response) => {
     }
 });
 
-server.listen(4000);
+server.listen(4000, () => {
+    console.log("Servidor corriendo en http://localhost:4000")
+});
