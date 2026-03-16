0bc17e4c5894382f8efdf9586cbaa88d264e30fe
Darío A. <dario.uribe.bss@gmail.com>
2026-03-15 22:17:01 -0600

    feat: add user registration with password hashing and validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,63 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user.model');
+
 exports.get_login = (req, res) => {
     res.render('login', {
         pagina: 'Login'
     });
 }
 
-exports.post_login = (req, res) => {
-    req.session.username = req.body.username;
-    res.redirect('/song/list')
+exports.post_login = (req, res, next) => {
+    const { username, password } = req.body;
+
+    User.findByName(username)
+        .then(([rows]) => {
+            console.log('Rows encontrados:', rows);
+            if (rows.length === 0) {
+            }
+            const user = rows[0];
+            return bcrypt.compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(error => {
+                            if (error) return next(error);
+                            res.redirect('/song/list');
+                        });
+                    }
+                    res.redirect('/login');
+                });
+        })
+        .catch(error => {
+            console.log(error);
+            next(error);
+        });
+};
+
+exports.get_signup = (req, res, next) => {
+    res.render('signup', {
+        pagina: 'Sign Up'
+    })
 }
+
+exports.post_signup = (req, res, next) => {
+    const {username, password} = req.body;
+    if (!username){
+        return res.redirect('/user/signup');
+    } else if (!password) {
+        return res.redirect('/user/signup');
+    }
+    User.create (username, password)
+    .then(() => {
+        res.redirect('/user/login');
+    })
+    .catch((error) => {
+        console.log(error);
+        next(error);
+    })
+}
+
 
 exports.get_logout = (req, res) => {
     req.session.destroy(() => {
--- routes/song.routes.js
+++ routes/song.routes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../middleware/is-auth');
 
 const songsController = require ('../controllers/songs.controller')
 
-router.get('/list', songsController.get_list);
-router.get('/new', songsController.get_new);
-router.post('/new', songsController.post_new);
-router.get('/detail/:id', songsController.get_detail);
-router.get('/thanks', songsController.get_thanks);
-router.get('/update/:id', songsController.get_update);
-router.post('/update/:id', songsController.post_update);
+router.get('/list', isAuth, songsController.get_list);
+router.get('/new', isAuth, songsController.get_new);
+router.post('/new', isAuth, songsController.post_new);
+router.get('/detail/:id', isAuth, songsController.get_detail);
+router.get('/thanks', isAuth, songsController.get_thanks);
+router.get('/update/:id', isAuth, songsController.get_update);
+router.post('/update/:id', isAuth, songsController.post_update);
 
 module.exports = router;
