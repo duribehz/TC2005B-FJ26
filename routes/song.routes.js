889c8a668a4d28b67986286c6367401a3bfb4b7b
duribehz <duribehz@gmail.com>
2026-04-19 20:54:48 -0600

    add upload middleware in new song route

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const isAuth = require('../middleware/is-auth');
 const isAdmin  = require('../middleware/is-admin');
+const upload = require('../middleware/upload');
 const songsController = require ('../controllers/songs.controller')
 
 router.get('/list', isAuth, songsController.get_list);
 router.get('/new', isAuth, songsController.get_new);
-router.post('/new', isAuth, songsController.post_new);
+router.post('/new', isAuth, upload.single('imagen'), songsController.post_new);
 router.get('/detail/:id', isAuth, songsController.get_detail);
 router.get('/thanks', isAuth, songsController.get_thanks);
 router.get('/update/:id', isAuth, isAdmin,songsController.get_update);
